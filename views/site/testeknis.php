12fe7776a0480a50f7d2cce528ddf66f96d5d3ed
m rizky ramadhan <gikbasembrani@gmail.com>
2024-04-19 00:37:14 +0700

    font size halaman testeknis

--- views/site/testeknis.php
+++ views/site/testeknis.php
@@ -1,6 +1,7 @@
-<html><p?>1. Tulislah program sederhana dalam PHP atau JavaScript yang mencetak angka dari 1 hingga 100. Namun, untuk kelipatan 3, cetak "Fizz" (bukan angka), dan untuk kelipatan 5, cetak "Buzz". Untuk angka yang merupakan kelipatan baik 3 dan 5, cetak "FizzBuzz". Sertakan kode yang sudah dibuat!</p>
+<html><p style="font-size:18px">1. Tulislah program sederhana dalam PHP atau JavaScript yang mencetak angka dari 1 hingga 100. Namun, untuk kelipatan 3, cetak "Fizz" (bukan angka), dan untuk kelipatan 5, cetak "Buzz". Untuk angka yang merupakan kelipatan baik 3 dan 5, cetak "FizzBuzz". Sertakan kode yang sudah dibuat!</p>
 <br>
-<p>Kode yang digunakan:</p>
+
+<p style="font-size:18px">Kode yang digunakan:</p>
 <p style="text-align:center;"><pre>
 for($x=1; $x<=100; $x++){
 	if($x%5!=0 && $x%3!=0){
@@ -15,9 +16,9 @@ for($x=1; $x<=100; $x++){
 	}
 	echo "&ltbr&gt";
 }</pre></p>
-<p>Program akan menghitung 1 sampai 100 dalam variabel $x. Jika $x tidak habis dibagi 3 dan tidak habis dibagi 5 maka $x akan dicetak. 
+<p style="font-size:18px">Program akan menghitung 1 sampai 100 dalam variabel $x. Jika $x tidak habis dibagi 3 dan tidak habis dibagi 5 maka $x akan dicetak. 
 Namun jika $x habis dibagi tiga maka program akan mencetak Fizz, dan jika $x habis dibagi 5 program akan mencetak Buzz.</p>
-<p><b>Hasil:</b></p>
+<p style="font-size:18px"><b>Hasil:</b></p>
 <?php
 	for($x=1; $x<=100; $x++){
 		if($x%5!=0 && $x%3!=0){
@@ -36,9 +37,9 @@ Namun jika $x habis dibagi tiga maka program akan mencetak Fizz, dan jika $x hab
 
 <br>
 
-<p>2. Tulislah fungsi dalam PHP atau JavaScript yang menghasilkan 25 angka pertama dalam deret Fibonacci. Sertakan kode yang dibuat beserta Penjelasannya!</p>
+<p style="font-size:18px">2. Tulislah fungsi dalam PHP atau JavaScript yang menghasilkan 25 angka pertama dalam deret Fibonacci. Sertakan kode yang dibuat beserta Penjelasannya!</p>
 <br>
-<p>Kode yang digunakan:</p>
+<p style="font-size:18px">Kode yang digunakan:</p>
 <p><pre>
 $sebelum=0;$sekarang=1; 
 echo $sebelum+$sekarang."&ltbr&gt";
@@ -49,11 +50,11 @@ while($sebelum+$sekarang<=25){
 	$sebelum=$sekarang;
 	$sekarang=$sekarang+$simpan;
 }</pre>
-<p>Konsep dari deret Fibonacci adalah deretan angka yang diawali dengan 1, 1, kemudian setiap angka setelahnya merupakan hasil penjumlahan dari 2 angka sebelumnya. 
+<p style="font-size:18px">Konsep dari deret Fibonacci adalah deretan angka yang diawali dengan 1, 1, kemudian setiap angka setelahnya merupakan hasil penjumlahan dari 2 angka sebelumnya. 
 Maka dua angka pertama disimpan dalam variabel $sebelum dan $sekarang. Selanjutnya terdapat looping penjumlahan dari $sebelum ditambah $sekarang yang akan berhenti sebelum penjumlahan kedua variabel tersebut mencapai 25 atau lebih.
 Setelah penjumlahan kedua variabel tersebut dicetak, nilai dari $sebelum akan disimpan dalam variabel sementara $simpan. Variabel $sebelum akan diubah nilainya menjadi sama dengan $sekarang.
 Lalu variabel $sekarang akan ditambahkan dengan nilai yang tadi disimpan di dalam variabel $simpan.</p>
-<p><b>Hasil:</b></p>
+<p style="font-size:18px"><b>Hasil:</b></p>
 <?php	
 	$sebelum=0;$sekarang=1; 
 	echo $sebelum+$sekarang."<br>";
@@ -68,9 +69,9 @@ Lalu variabel $sekarang akan ditambahkan dengan nilai yang tadi disimpan di dala
 
 <br>
 
-<p>3. Terdapat tiga tabel di database: Donation, Donor, dan Branch. Tabel Donation berisi catatan donasi dengan kolom seperti donation_id, donor_id, branch_id, donation_date, dan amount. Tulis query SQL yang menghitung jumlah donasi bulanan total per donor per cabang. Sertakan query SQL yang dbuat!</p>
+<p style="font-size:18px">3. Terdapat tiga tabel di database: Donation, Donor, dan Branch. Tabel Donation berisi catatan donasi dengan kolom seperti donation_id, donor_id, branch_id, donation_date, dan amount. Tulis query SQL yang menghitung jumlah donasi bulanan total per donor per cabang. Sertakan query SQL yang dbuat!</p>
 
-Pertama, buat ketiga tabel dahulu.
+<p style="font-size:18px">Pertama, buat ketiga tabel dahulu.</p>
 <pre>
 CREATE TABLE Donor (
     id int,
@@ -90,7 +91,26 @@ CREATE TABLE Donation (
 	branch_id int
 );</pre>
 
-Kemudian query untuk menghitung total donasi tiap donor di tiap cabang per bulan:<br>
+<p style="font-size:18px">Kemudian isi data ke dalam database. COntohnya dengan nilai berikut.</p>
+
+<pre>INSERT INTO `branch` (`id`, `name`) VALUES
+(1, 'Branch Pekanbaru'),
+(2, 'Branch Alor'),
+(3, 'Branch Pontianak');
+
+INSERT INTO `donor` (`id`, `name`) VALUES
+(1, 'Boboiboy'),
+(2, 'Ronaldo');
+
+INSERT INTO `donation` (`id`, `amount`, `donation_date`, `donor_id`, `branch_id`) VALUES
+(1, 3000000, '2024-04-01', 2, 1),
+(2, 50000, '2024-04-11', 1, 2),
+(3, 2500000, '2011-06-05', 2, 2),
+(4, 2400000, '2024-04-10', 2, 2),
+(5, 1200000, '2024-03-10', 2, 2),
+(6, 25000, '2024-04-03', 1, 2);</pre>
+
+<p style="font-size:18px">Kemudian query untuk menghitung total donasi tiap donor di tiap cabang per bulan adalah:<br></p>
 SELECT SUM(donation.amount), donor.name as donor_name, branch.name as branch_name, MONTH(donation_date) AS month, YEAR(donation_date) AS year
 FROM donation
 JOIN donor ON donation.donor_id = donor.id
